1776ffdcaafccf1de960bf37aa38ed2e0940cb3d
cherish-an1 <eanozie98@gmail.com>
2020-08-28 15:50:22 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//open and close menu bar
+function w3_open() {
+    document.getElementById ("mysidebar").style.display = "block";
+    document.getElementById ("mysidebar").style.width = "100%" ;
+ 
+}
+function w3_close() {
+    document.getElementById ("mysidebar").style.display = "none" ;
+}
